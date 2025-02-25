4ba7b821b2bf807d9161fccefb8b63de612b7fe6
k-daiguji <hikatteru20@gmail.com>
2025-02-25 21:09:45 +0900

    Show japan standard time

---
+++ src/base/countries/index.ts
@@ -0,0 +1,18 @@
+import type { Plugin } from "vue";
+
+import { Japan } from "@/base/countries/japan";
+
+export const countries = {
+  install(app) {
+    app.provide<CountryConstructor[]>("countries", [Japan]);
+  },
+} satisfies Plugin;
+
+export interface CountryConstructor {
+  new (): Country;
+  get name(): string;
+}
+
+export interface Country {
+  get now(): string;
+}
