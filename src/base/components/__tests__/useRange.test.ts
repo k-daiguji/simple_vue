04101df05f174bfd04dda7b5f3355f6cfcecd35b
k-daiguji <hikatteru20@gmail.com>
2025-03-25 23:04:04 +0900

    Remove unnecessary describe

--- src/base/components/__tests__/useRange.test.ts
+++ src/base/components/__tests__/useRange.test.ts
@@ -1,55 +1,53 @@
-import { describe, test } from "vitest";
+import { test } from "vitest";
 import { ref } from "vue";
 
 import { useRange } from "../useRange";
 
-describe("Number constructor extensions", () => {
-  test.for([
-    [-0.1, -0.2],
-    [-0.2, -0.3],
-    [-0.3, -0.4],
-    [-0.4, -0.5],
-    [-0.5, -0.6],
-    [-0.6, -0.7],
-    [-0.7, -0.8],
-    [-0.8, -0.9],
-    [-0.9, -1],
-    [-1, -1],
-  ])("decrement(%s -> %s)", ([initializeValue, expected], { expect }) => {
-    const input = ref(initializeValue);
-    const { decrement } = useRange(input, 1, -1, 0, 0.1);
+test.for([
+  [-0.1, -0.2],
+  [-0.2, -0.3],
+  [-0.3, -0.4],
+  [-0.4, -0.5],
+  [-0.5, -0.6],
+  [-0.6, -0.7],
+  [-0.7, -0.8],
+  [-0.8, -0.9],
+  [-0.9, -1],
+  [-1, -1],
+])("decrement(%s -> %s)", ([initializeValue, expected], { expect }) => {
+  const input = ref(initializeValue);
+  const { decrement } = useRange(input, 1, -1, 0, 0.1);
 
-    decrement();
+  decrement();
 
-    expect(input.value).toBe(expected);
-  });
+  expect(input.value).toBe(expected);
+});
 
-  test.for([
-    [0.1, 0.2],
-    [0.2, 0.3],
-    [0.3, 0.4],
-    [0.4, 0.5],
-    [0.5, 0.6],
-    [0.6, 0.7],
-    [0.7, 0.8],
-    [0.8, 0.9],
-    [0.9, 1],
-    [1, 1],
-  ])("increment(%s -> %s)", ([initializeValue, expected], { expect }) => {
-    const input = ref(initializeValue);
-    const { increment } = useRange(input, 1, -1, 0, 0.1);
+test.for([
+  [0.1, 0.2],
+  [0.2, 0.3],
+  [0.3, 0.4],
+  [0.4, 0.5],
+  [0.5, 0.6],
+  [0.6, 0.7],
+  [0.7, 0.8],
+  [0.8, 0.9],
+  [0.9, 1],
+  [1, 1],
+])("increment(%s -> %s)", ([initializeValue, expected], { expect }) => {
+  const input = ref(initializeValue);
+  const { increment } = useRange(input, 1, -1, 0, 0.1);
 
-    increment();
+  increment();
 
-    expect(input.value).toBe(expected);
-  });
+  expect(input.value).toBe(expected);
+});
 
-  test("reset", ({ expect }) => {
-    const input = ref(0);
-    const { reset } = useRange(input, 10, 0, 5);
+test("reset", ({ expect }) => {
+  const input = ref(0);
+  const { reset } = useRange(input, 10, 0, 5);
 
-    reset();
+  reset();
 
-    expect(input.value).toBe(5);
-  });
+  expect(input.value).toBe(5);
 });
