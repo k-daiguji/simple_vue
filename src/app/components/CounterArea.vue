360caa3ec5e8487b2a26ad78a582b0df44f8bc3b
k-daiguji <hikatteru20@gmail.com>
2025-03-25 23:00:21 +0900

    Add useRange

--- src/app/components/CounterArea.vue
+++ src/app/components/CounterArea.vue
@@ -2,17 +2,14 @@
 import { ref } from "vue";
 
 import BaseButton from "@/base/components/BaseButton.vue";
+import { useRange } from "@/base/components/useRange";
 
 const counter = ref(0);
+const { decrement, increment, reset } = useRange(counter, 10, -10, 0);
 const buttons = new Map([
-  ["Increment", () => counter.value++],
-  ["Decrement", () => counter.value--],
-  [
-    "Reset",
-    () => {
-      counter.value = 0;
-    },
-  ],
+  ["Increment", increment],
+  ["Decrement", decrement],
+  ["Reset", reset],
 ]);
 </script>
 
